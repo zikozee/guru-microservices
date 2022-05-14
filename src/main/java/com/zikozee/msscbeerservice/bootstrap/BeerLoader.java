72c41eb33602cc63c9c91af1174bfd28df91576b
ezekiel.eromosei <ezekiel.eromosei@gmail.com>
2022-05-14 07:34:01 +0100

    Added BootStrap class to initialize Beer objects closes #7

---
+++ src/main/java/com/zikozee/msscbeerservice/bootstrap/BeerLoader.java
@@ -0,0 +1,49 @@
+package com.zikozee.msscbeerservice.bootstrap;
+
+import com.zikozee.msscbeerservice.domain.Beer;
+import com.zikozee.msscbeerservice.repositories.BeerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+/**
+ * @author: Ezekiel Eromosei
+ * @created: 14 May 2022
+ */
+
+@Component
+@RequiredArgsConstructor
+public class BeerLoader implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadBeerObjects();
+    }
+
+    private void loadBeerObjects() {
+        if(beerRepository.count() == 0L){
+
+            beerRepository.save(Beer.builder()
+                            .beerName("Mango Bobs")
+                            .beerStyle("IPA")
+                            .quantityToBrew(200)
+                            .minOnHand(12)
+                            .upc(337010000001L)
+                            .price(BigDecimal.valueOf(12.95))
+                    .build());
+
+            beerRepository.save(Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle("PALE_ALE")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(337010000002L)
+                    .price(BigDecimal.valueOf(11.95))
+                    .build());
+        }
+    }
+}
