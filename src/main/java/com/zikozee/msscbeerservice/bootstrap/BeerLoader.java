781f69c4a95dcf830aeb81a641885603079d91e9
ezekiel.eromosei <ezekiel.eromosei@gmail.com>
2022-05-30 22:02:46 +0100

    Adding standard UPCs closes #17

--- src/main/java/com/zikozee/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/com/zikozee/msscbeerservice/bootstrap/BeerLoader.java
@@ -17,6 +17,9 @@ import java.math.BigDecimal;
 @RequiredArgsConstructor
 public class BeerLoader implements CommandLineRunner {
 
+    public static final String BEER_1_UPC = "0631234200036";
+    public static final String BEER_2_UPC = "0631234300019";
+    public static final String BEER_3_UPC = "0083783375213";
     private final BeerRepository beerRepository;
 
     @Override
@@ -32,7 +35,7 @@ public class BeerLoader implements CommandLineRunner {
                             .beerStyle("IPA")
                             .quantityToBrew(200)
                             .minOnHand(12)
-                            .upc(337010000001L)
+                            .upc(BEER_1_UPC)
                             .price(BigDecimal.valueOf(12.95))
                     .build());
 
@@ -41,7 +44,16 @@ public class BeerLoader implements CommandLineRunner {
                     .beerStyle("PALE_ALE")
                     .quantityToBrew(200)
                     .minOnHand(12)
-                    .upc(337010000002L)
+                    .upc(BEER_2_UPC)
+                    .price(BigDecimal.valueOf(11.95))
+                    .build());
+
+            beerRepository.save(Beer.builder()
+                    .beerName("No Hammers on The bar")
+                    .beerStyle("PALE_ALE")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(BEER_3_UPC)
                     .price(BigDecimal.valueOf(11.95))
                     .build());
         }
--- src/test/java/com/zikozee/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/com/zikozee/msscbeerservice/web/controller/BeerControllerTest.java
@@ -1,22 +1,27 @@
 package com.zikozee.msscbeerservice.web.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zikozee.msscbeerservice.services.BeerService;
 import com.zikozee.msscbeerservice.web.model.BeerDto;
 import com.zikozee.msscbeerservice.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
+import static com.zikozee.msscbeerservice.bootstrap.BeerLoader.BEER_1_UPC;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(BeerController.class)
+@WebMvcTest(BeerController.class) // this does not bring up the entire context hence a mockbean
 class BeerControllerTest {
 
     public static final String FORWARD_SLASH = "/";
@@ -27,8 +32,12 @@ class BeerControllerTest {
     @Autowired
     ObjectMapper mapper;
 
+    @MockBean
+    BeerService beerService;
+
     @Test
     void getBeerById() throws Exception {
+        given(beerService.getById(any())).willReturn(getValidBeerDto());
 
         mockMvc.perform(get(FORWARD_SLASH + BeerController.BASE_URL + UUID.randomUUID().toString())
                 .accept(MediaType.APPLICATION_JSON))
@@ -49,6 +58,9 @@ class BeerControllerTest {
 
     @Test
     void updateBeerId() throws Exception {
+
+        given(beerService.getById(any())).willReturn(getValidBeerDto());
+
         BeerDto beerDto = getValidBeerDto();
         String beerDtoJson = mapper.writeValueAsString(beerDto);
 
@@ -64,7 +76,7 @@ class BeerControllerTest {
                 .beerName("My Beer")
                 .beerStyle(BeerStyleEnum.ALE)
                 .price(BigDecimal.valueOf(2.99))
-                .upc(123123123123L)
+                .upc(BEER_1_UPC)
                 .build();
     }
 }
