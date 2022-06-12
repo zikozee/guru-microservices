6ea34479ffb0d4a08ae831639597c3a99834c47c
ezekiel.eromosei <ezekiel.eromosei@gmail.com>
2022-06-12 19:55:35 +0100

    restructured project

--- src/main/java/com/zikozee/msscbeerservice/web/controller/NotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.zikozee.msscbeerservice.web.controller;
-
-/**
- * @author: Ezekiel Eromosei
- * @created: 30 May 2022
- */
-
-public class NotFoundException extends RuntimeException {
-}
